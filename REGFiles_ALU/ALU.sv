803813d26a330bdd46acc3dbeb737cf379de7c2b
omarbengacem <omar.ben-gacem20@imperial.ac.uk>
2022-11-20 13:38:04 +0000

    Added Comments

--- REGFiles_ALU/ALU.sv
+++ REGFiles_ALU/ALU.sv
@@ -1,6 +1,14 @@
+/*
+The ALU unit can perform five different operations based on the opcode (ALUCtrl). See the Readme for the details
+On the Opcode. The Two inputs must have the same input data width, as the ALU does not sign extend. The ALU is 
+asyncronous and thus does not have a clocked input
+*/
+
 module ALU #(
 
+    //width of both operands
     parameter DATA_WIDTH = 32,
+    //width of opcode
     parameter ALUCTRL_WIDTH = 3
 
 ) (
@@ -13,7 +21,8 @@ module ALU #(
 
 )
 
-//combinational logic so not using always_ff
+//combinational logic so not using always_ff, these are all the different opcodes. SLT sets the output SUM to 1 if
+//op1 > op2, else sets it to 0, EQ is 1 if both the operands equal eachother.
 
 if (ALUCtrl == 000) SUM = ALUOP1 + ALUOP2; //add
 if (ALUCtrl == 001) SUM = ALUOP1 - ALUOP2; //subtract
@@ -23,6 +32,8 @@ if (ALUCtrl == 101) begin //SLT (set less than)
     if(ALUOP1 > ALUOP2) Sum = 32'b1;
     else Sum = 32'b0;
 end
+
+//setting EQ
 if (ALUOP1 == ALUOP2) EQ = 0'b1;
  
 
--- REGFiles_ALU/ALUMux.sv
+++ REGFiles_ALU/ALUMux.sv
@@ -1,19 +1,28 @@
+/*
+The Mux is used to select the seond operand of the ALU Unit, it can either be in0, which
+is the output of the second register address (RA2 -> RD2) or an immidiate (sign extended)
+operand for in1. The multiplexer is entirely combinational and not syncronous with a clock.
+*/
+
 module ALUMux #(
 
+    //the width of the data for the multiplexer
     parameter DATA_WIDTH = 32
 
 ) (
+    //ALUsrc is either 0 for in0 or 1 for in1
     input logic                     ALUsrc,
     input logic  [DATA_WIDTH-1:0]   in0,
     input logic  [DATA_WIDTH-1:0]   in1,
     output logic [DATA_WIDTH-1:0]   Out
 );
 
-always_ff @(posedge clk) begin
 
-    if(ALUsrc) Out <= in1
-    else Out <= in0;
+//combinational so not doing always_ff
 
-end
+//if ALUsrc = 0, Out = in0, or Out = in1
+if(ALUsrc) Out = in1
+else Out = in0;
+
 
 endmodule
