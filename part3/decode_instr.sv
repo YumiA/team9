667b35c79245f5ef8b6c481e1939ae48d48c117f
yomna <yomihamody@gmail.com>
2022-11-23 14:18:55 +0000

    Commit instruction decode block

--- part3/decode_instr.sv
+++ part3/decode_instr.sv
@@ -1,6 +1,14 @@
-module decode_instr (
-    input logic [31:0]      inst,
+module decode_instr #(
+    paramater INSTR_WIDTH = 32
+)(
+    input logic [31:0]      instr,
     output logic [4:0]      rs1,
     output logic [4:0]      rs2,
     output logic [4:0]      rsd
-)
+);
+
+always_comb begin
+    rs1 = instr[19:15]
+    rs2 = instr[24:20]
+    rsd = instr[11:7]
+end
