c7255936d5c0b028973b59a612a741583753e232
mubaarak2002 <106095474+mubaarak2002@users.noreply.github.com>
2022-11-29 22:59:53 +0000

    bug fixes

--- Simulation/sign_extend.sv
+++ Simulation/sign_extend.sv
@@ -2,16 +2,15 @@ module sign_extend#(
     parameter DATA_WIDTH = 32
 )(
     input logic                  ImmSrc,
-    input logic  [31:20]         Imm31to20,
-    input logic  [11:7]          Imm11to7,
+    input logic  [DATA_WIDTH-1:0]     instr,
     output logic [DATA_WIDTH-1:0]     ImmOp
 );
 
 always_comb begin
     if (ImmSrc == 0)
-        ImmOp = {{20{Imm31to20[31]}}, Imm31to20};
+        ImmOp = {{20{instr[31]}}, instr[31:20]};
     else 
-        ImmOp = {{20{Imm31to20[31]}}, Imm31to20[31], Imm11to7[7], Imm31to20[30:25], Imm11to7[11:8]};
+        ImmOp = {{20{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8]};
     end
 
 endmodule
